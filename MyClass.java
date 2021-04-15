f8ad2e546335a8341b0b979c8843824043a849ed
shj971219 <shj971219@gmail.com>
2021-04-15 14:15:49 +0900

    파일 읽기 성공

---
+++ MyClass.java
@@ -0,0 +1,28 @@
+public class MyClass {
+    public static void main(String args[]) {
+        int a = -123;
+        int b = 012;
+        boolean abc_123 = true;
+        boolean 0abc0 = false;
+        char 123if = 'a';
+        char 123int001 = ' ';
+        char c = '1';
+        String d = "Hello java int boolean char String";
+        
+        if(a==-123)
+        {
+            a=a+2--2;
+        }
+        if(c!='0')
+        {
+            c='1';
+        }
+        while(a<=0)
+        {
+            a = a + (2*4)/2;
+        }
+        int else123, asd123, _123as = 54;
+        
+        return -0;
+    }
+}

83cade6c7edbd37aadc1c246bf0306ed20faac89
shj971219 <shj971219@gmail.com>
2021-06-02 16:44:22 +0900

    define token

--- MyClass.java
+++ MyClass.java
@@ -1,29 +1,22 @@
 public class MyClass {
-    public static void main(String args[]) {
+    public static void main() {
         int a = -123;
-        int b = 012;
         boolean abc_123 = true;
-        boolean 0abc0 = false;
-        char 123if = 'a';
-        char 123int001 = ' ';
         char c = '1';
         String d = "Hello java int boolean char String";
         
-        if(a==-123)
+        if(true < false)
         {
-            a=a+2--2;
+            a= a+2-2;
         }
         else
         {
-            if(c!='1')
-                c='1';
+            c = '3';
         }
-        while(a<=0)
+        while(true >= false)
         {
             a = a + (2*4)/2;
-        }
-        int else123, asd123, _123as = 54;
-        
-        return -0;
+        }      
+        return 0;
     }
 }
